9ebd4a113957a50c1135b9af6c4112cdd9da9fd3
Bence Balogh <benceeok@gmail.com>
2022-01-07 23:26:45 +0100

    Update __init__.py

--- src/dewloosh/geom/__init__.py
+++ src/dewloosh/geom/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.dev7"
+__version__ = "0.0.dev8"
 __description__ = "A pure python package to build, manipulate and analyze polygonal meshes."
 import dewloosh.geom.topo.tr as tr
 import dewloosh.geom.topo.topodata as topodata
